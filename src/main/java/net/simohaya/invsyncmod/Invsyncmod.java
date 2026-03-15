ff7dd40d335afac61a8e162767775cc9c04624ad
Simohayhe <166674587+Simohayhe@users.noreply.github.com>
2026-03-15 19:35:50 +0900

    Initial commit

---
+++ src/main/java/net/simohaya/invsyncmod/Invsyncmod.java
@@ -0,0 +1,10 @@
+package net.simohaya.invsyncmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Invsyncmod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
